a172c445faf226eec91870c49a2bbf9a4a03ccc1
limbrasonadev <ehliforeducation@gmail.com>
2025-09-03 22:03:59 +0800

    Change the title into pleasant title

--- sendemail_form.php
+++ sendemail_form.php
@@ -3,7 +3,7 @@
 <head>
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1" />
-  <title>Send Email</title>
+  <title>Sending Email</title>
   <style>
 
     * {
